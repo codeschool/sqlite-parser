ce7d4df531be37f9a27f3e58bf7b0d7647b30164
Nick Wronski <nick@codeschool.com>
2015-06-10 11:32:36 -0400

    spec for transaction syntax. refs #1

---
+++ test/sql/basicTransaction.sql
@@ -0,0 +1,14 @@
+BEGIN IMMEDIATE TRANSACTION
+CREATE TABLE foods (
+  id int PRIMARY KEY,
+  item varchar(50),
+  size varchar(15),
+  price int
+);
+
+INSERT INTO foods (item, size, id, price)
+  SELECT 'banana', size, null, price
+  FROM bananas
+  WHERE color != 'red'
+
+END TRANSACTION
