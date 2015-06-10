9a4ba18bcf5d1631286a3d10a52adc8a0df4f0ae
Nick Wronski <nick@codeschool.com>
2015-06-10 11:31:45 -0400

    polyfill es6-promise library if not present. refs #1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-var Promise     = require('promise/lib/es6-extensions'),
-    sqlParser   = require('./sql-parser');
+var prom        = require('promise/lib/es6-extensions'),
+    parser      = require('./sql-parser'),
+    top         = typeof global !== "undefined" && global !== null ? global : self,
+    p           = top.Promise;
+
+/** @note Taken from jakearchibald/es6-promise polyfill.js */
+if (p && Object.prototype.toString.call(p.resolve()) === '[object Promise]' && !p.cast) {
+  // Use existing Promise
+  prom = p;
+}
 
 function sqlQueryParser(source) {
-  return new Promise(function(resolve) {
-    resolve(sqlParser.parse(source));
+  return new prom(function(resolve) {
+    resolve(parser.parse(source));
   });
 }
-sqlQueryParser.NAME = "sql-query-parser";
-sqlQueryParser.VERSION = "0.0.2";
+sqlQueryParser['NAME'] = "sql-query-parser";
+sqlQueryParser['VERSION'] = "0.0.2";
 
 module.exports = sqlQueryParser;
