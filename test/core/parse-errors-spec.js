dd250590e521bf8d37255ef3ccf78b1eb775c665
Nick Wronski <nick@codeschool.com>
2015-12-18 13:46:21 -0500

    Updated pegjs to latest master.

--- test/core/parse-errors-spec.js
+++ test/core/parse-errors-spec.js
@@ -2,7 +2,7 @@ describe('parse errors', function() {
 
   it('parse error 1', function(done) {
     tree.error({
-      'message': 'Expected Block Comment, Line Comment, New Line, Semicolon, Whitespace or end of input but "W" found.'
+      'message': 'Expected Block Comment, Line Comment, New Line, Semicolon, Whitespace or end of input.'
     }, this, done);
   });
 
