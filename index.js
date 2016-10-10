cf2eea3c3bcb9dd7017d0e44b105757037c4803d
Nick Wronski <nick@codeschool.com>
2016-10-10 00:15:50 -0400

    Do not catch errors inside user callback.
    When parser is used asynchronously, do not capture the error
    within the try catch block. Also, do not run an error thrown by
    the parser through tracer smartError() unless it is a
    PegSyntaxError.

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 /**
  * sqlite-parser
  */
-import { parse } from './lib/parser';
+import { parse, SyntaxError as PegSyntaxError } from './lib/parser';
 import { Tracer } from './lib/tracer';
 
 export default function sqliteParser(source, callback) {
   const t = Tracer();
   const isAsync = typeof callback === 'function';
   const opts = { 'tracer': t };
-  // Async
   if (isAsync) {
+    // Async
     setTimeout(function () {
+      let result;
       try {
-        callback(null, parse(source, opts));
+        result = parse(source, opts);
       } catch (e) {
-        callback(t.smartError(e));
+        callback(e instanceof PegSyntaxError ? t.smartError(e) : e);
       }
+      callback(null, result);
     }, 0);
-    return;
-  }
-  // Sync
-  try {
-    return parse(source, opts);
-  } catch (e) {
-    throw t.smartError(e);
+  } else {
+    // Sync
+    try {
+      return parse(source, opts);
+    } catch (e) {
+      throw e instanceof PegSyntaxError ? t.smartError(e) : e;
+    }
   }
 };
 
