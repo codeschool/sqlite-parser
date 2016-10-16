9ecda8b35860d259881ee3b317e2f5a8b92bbb56
Nick Wronski <nick@codeschool.com>
2016-10-16 11:09:10 -0400

    Add SingleNodeTransform to browser shim. Refs #27

--- src/streaming-shim.js
+++ src/streaming-shim.js
@@ -4,3 +4,9 @@ export class SqliteParserTransform {
     throw new Error('SqliteParserTransform is not available in this environment');
   }
 }
+
+export class SingleNodeTransform {
+  constructor(options) {
+    throw new Error('SingleNodeTransform is not available in this environment');
+  }
+}
